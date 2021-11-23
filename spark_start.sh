6e1b1144f71033c090dd6a47cbaacda500efb891
Jonathan Armstrong <8119806+jonathan-armstrong-303@users.noreply.github.com>
2021-11-22 20:18:11 -0700

    cleaning up verbiage

--- spark_start.sh
+++ spark_start.sh
@@ -4,13 +4,11 @@ sudo su -
 cd /usr/local/spark
 ./sbin/start-all.sh
 
-
 # stop spark cluster [on master]
 cd /usr/local/spark
 ./sbin/stop-all.sh
 
-# Check whether services have been started 
-# You should see output something like the following:
+# Check whether services have been started -- should see e.g., output like this:
 # 65530 Jps
 # 65468 Worker
 # 65373 Master
